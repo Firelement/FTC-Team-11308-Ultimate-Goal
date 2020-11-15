0baab0956a0cfcd4369f9f5d3855232ae4bbf253
Josh <snickers07@comcast.net>
2020-11-15 13:11:52 -0700

    Renamed Program Title To Be More Clear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamTestProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamTestProgram.java
@@ -51,7 +51,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
+@TeleOp(name = "Webcam Test Program", group = "Concept")
 public class WebcamTestProgram extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
