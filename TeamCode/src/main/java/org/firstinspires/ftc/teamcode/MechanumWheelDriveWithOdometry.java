40b154e2ee93b2ad347a95cdca751ac8ce186a65
Josh <snickers07@comcast.net>
2020-12-06 14:47:32 -0700

    Created New Odometry Program
    Just coppied mechanum wheel drive program and added some telelmetry data for testing the odometry wheel.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumWheelDriveWithOdometry.java
@@ -0,0 +1,129 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Mechanum Wheel Driver Control", group="Linear Opmode")
+@Disabled
+public class MechanumWheelDriveWithOdometry extends LinearOpMode {
+
+    // Declare Motor Classes
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive;
+    private DcMotor rightFrontDrive;
+    private DcMotor leftRearDrive;
+    private DcMotor rightRearDrive;
+    private DcMotor OdometryWheelX;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //Initialize the Motors
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        leftRearDrive  = hardwareMap.get(DcMotor.class, "left_rear_drive");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "right_rear_drive");
+        OdometryWheelX = hardwareMap.get(DcMotor.class, "Odometry_Wheel_X");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        // I believe that motors on the same side of the robot need to run in the same direction
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        OdometryWheelX.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to hold power level
+            double leftFrontPower;
+            double rightFrontPower;
+            double leftRearPower;
+            double rightRearPower;
+
+            // Setup variables to hold the Game pad inputs
+            double drive;
+            double rotate;
+            // positive rotation is in the clockwise direction
+            double strafe;
+
+            // Assign the Game pad inputs
+            // These can be changed to what ever the driver wants it to be
+            //some of these may need to be made negative depending  on the gamepad
+            drive = gamepad1.right_stick_y;
+            rotate = gamepad1.right_stick_x;
+            strafe = gamepad1.left_stick_x;
+
+            // Assign the power variables to the Game pad inputs
+            // currently strafing is not implemented
+            leftFrontPower = drive + rotate + strafe;
+            leftRearPower = drive + rotate - strafe;
+            rightFrontPower = drive - rotate - strafe;
+            rightRearPower = drive - rotate + strafe;
+
+            // make sure that the power variables are within -1.0 and 1.0
+            leftFrontPower    = Range.clip(leftFrontPower, -1.0, 1.0) ;
+            rightFrontPower   = Range.clip(rightFrontPower, -1.0, 1.0) ;
+            leftRearPower     = Range.clip(leftRearPower,-1.0,1.0);
+            rightRearPower    = Range.clip(rightRearPower,-1.0,1.0);
+
+            // Send calculated power to wheels
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftRearDrive.setPower(leftRearPower);
+            rightRearDrive.setPower(rightRearPower);
+
+            telemetry.addData("Y-Position",OdometryWheelX.getCurrentPosition());
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Left Front Motor Power:",leftFrontPower);
+            telemetry.addData("Right Front Motor Power:",rightFrontPower);
+            telemetry.addData("Left Rear Motor Power:",leftRearPower);
+            telemetry.addData("Right Rear Motor Power:",rightRearPower);
+            telemetry.update();
+        }
+    }
+}
