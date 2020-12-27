4ac5ae5474febc193b7ddc486b638b336ad3ecb4
Josh <snickers07@comcast.net>
2020-12-27 14:14:03 -0700

    Added in skeleton code and comments for what needs to happen in each function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
@@ -18,7 +18,8 @@ public class AutoRed extends LinearOpMode {
     static final double COUNTS_PER_INCH = 531;
     static final double     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.4;
-
+    static final double INTAKE_WHEEL_SPEED = 1;
+    int rings = 0;
     @Override
     public void runOpMode() {
 
@@ -46,7 +47,7 @@ public class AutoRed extends LinearOpMode {
         //Detect Ring Stack - Detect Rings
 
         /*If: 4 rings
-        Power Shots  - Encoder Drive, Shoot Rings
+        Power Shots  - Shoot 1 ring 3 times
         Deposit wobble goal - Open Wobble Claw
         Grab Rings - Intake Wheel Drive
         Shoot high goal - Shoot Rings
@@ -56,7 +57,7 @@ public class AutoRed extends LinearOpMode {
         */
 
         /*If: 1 ring
-        Power Shots  - Encoder Drive, Shoot Rings
+        Power Shots  - Shoot 1 ring 3 times
         Deposit wobble goal - Open Wobble Claw
         Grab Ring - Intake Wheel Drive
         Shoot high goal - Shoot Rings
@@ -111,20 +112,56 @@ public class AutoRed extends LinearOpMode {
             }
         }
 
-        public void intakeWheelDrive(){
+        public void intakeWheelDrive(double speed, double inches, double timeoutS){
+            int newTarget;
+            // Ensure that the opmode is still active
+            if (opModeIsActive()) {
 
+                // Determine new target position, and pass to motor controller
+                newTarget = leftFrontDrive.getCurrentPosition() + (int) (inches * COUNTS_PER_INCH);
+                //reset runtime
+                runtime.reset();
+
+                if (leftFrontDrive.getCurrentPosition() > newTarget) { //If current position is larger than new target, robot needs to reverse
+
+                    while (leftFrontDrive.getCurrentPosition() >= newTarget && (runtime.seconds() < timeoutS) && opModeIsActive()) {
+                        leftFrontDrive.setPower(-speed);
+                        rightFrontDrive.setPower(-speed);
+                        leftRearDrive.setPower(-speed);
+                        rightRearDrive.setPower(-speed);
+                        //INTAKE WHEELS SPIN
+                    }
+                } else { //If current position is smaller than new target, robot goes forward
+                    while (leftFrontDrive.getCurrentPosition() <= newTarget && (runtime.seconds() < timeoutS) && opModeIsActive()) {
+                        leftFrontDrive.setPower(speed);
+                        rightFrontDrive.setPower(speed);
+                        leftRearDrive.setPower(speed);
+                        rightRearDrive.setPower(speed);
+                        //INTAKE WHEELS SPIN
+                    }
+                }
+
+                // Stop all motion;
+                leftFrontDrive.setPower(0);
+                rightFrontDrive.setPower(0);
+                leftRearDrive.setPower(0);
+                rightRearDrive.setPower(0);
+                //INTAKE WHEELS STOP
+                
+                //  sleep(250);   // optional pause after each move
+            }
         }
-        
+
         public int detectRings(){
-            int rings = 0;
+            //Webcam Detection changes rings to number of rings- defaults to 0
             return rings;
         }
 
-        public void shootRings(){
-
+        public void shootRings(int amountToShoot){
+            //Launch rings for specific amount of time, according to amount to shoot
         }
 
         public void wobbleClawOpen(){
-
+            //Set wobble claw position to be open
         }
     }
