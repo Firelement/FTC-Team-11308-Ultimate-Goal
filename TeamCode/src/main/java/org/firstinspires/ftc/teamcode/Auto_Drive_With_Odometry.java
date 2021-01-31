90796969f69898c087c9a2b389ccaa12497492f4
Josh <snickers07@comcast.net>
2021-01-31 15:24:24 -0700

    Demonstration of a commit
    Blah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Drive_With_Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Drive_With_Odometry.java
@@ -54,7 +54,7 @@ public class Auto_Drive_With_Odometry extends LinearOpMode {
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * 3.1415);*/
-    static final double COUNTS_PER_INCH = 531;
+    static final double COUNTS_PER_INCH = 1250;
     static final double     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.4;
 
